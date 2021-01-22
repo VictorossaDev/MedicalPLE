bd8273fba13de5554701047a5e83f01bf96694f2
Victor <victorossa.developer@gmail.com>
2021-01-22 10:51:48 -0500

    Inicio

---
+++ MedicalPLE/MedicalPLE.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MedicalPLE.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ MedicalPLE/MedicalPLE.Utilidades/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MedicalPLE.Utilidades
+{
+    public class Class1
+    {
+    }
+}
